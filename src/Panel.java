1a1f27cecfb4a09c525a31cb67225d439153795b
tretyakov3112 <tretyakov3112@mail.ru>
2021-06-12 21:06:26 +0300

    таймер

--- src/Panel.java
+++ src/Panel.java
@@ -19,6 +19,7 @@ public class Panel extends JPanel implements KeyEventDispatcher, MouseListener {
     boolean mousepressed = false;
     int mouseX = 0;
     int mouseY = 0;
+    long time1 = System.currentTimeMillis();
 
 
 
@@ -38,41 +39,45 @@ public class Panel extends JPanel implements KeyEventDispatcher, MouseListener {
 
     @Override
     public void paintComponent(Graphics g) {
+        System.out.println((time-time1)/1000);
+        long tmp = (time-time1)/1000;
         boolean shouldPlay = false;
         shouldPlay = mousepressed && (mouseX>442*1.5 && mouseX<582*1.5) && (mouseY>209*1.5 && mouseY<250*1.5);
         boolean shouldPause = false;
         shouldPause = mousepressed && (mouseX>Main.width/2 && mouseX<Main.width/2 + 50) && (mouseY>Main.height/20 && mouseY<Main.height/20+50);
-            long time1 = System.currentTimeMillis();
-            float dt = (time1 - time);
-            world.update(dt / 1000);
-            world.moveWalls();
-            time = time1;
-            world.background.draw(g);
-            world.sphere.draw(g);
-            g.drawImage(pauseImage, Main.width/2, Main.height/20, 50, 50, null);
-            for (int i = 1; i < world.wallCount - 1; i++) {
-                world.walls[i].draw(g);
+        long time1 = System.currentTimeMillis();
+        float dt = (time1 - time);
+        world.update(dt / 1000);
+        world.moveWalls();
+        time = time1;
+        world.background.draw(g);
+        world.sphere.draw(g);
+        g.drawImage(pauseImage, Main.width/2, Main.height/20, 50, 50, null);
+        for (int i = 1; i < world.wallCount - 1; i++) {
+            world.walls[i].draw(g);
+        }
+        Booster toRemove = null;
+        for (Booster booster : world.boosterList) {
+            if (booster.update(world.sphere)) {
+                toRemove = booster;
+            } else {
+                booster.draw(g);
             }
-            Booster toRemove = null;
-            for (Booster booster : world.boosterList) {
-                if (booster.update(world.sphere)) {
-                    toRemove = booster;
-                } else {
-                    booster.draw(g);
-                }
-            }
-            if (toRemove != null) {
-                world.boosterList.remove(toRemove);
-            }
+        }
+        if (toRemove != null) {
+            world.boosterList.remove(toRemove);
+        }
 
-            if (world.sphere.pos.y > Main.height && world.sphere.pos.x <= world.sphere.xPos1) {
-                g.drawImage(loseMenu, 0, 0, Main.width, Main.height, null);
-            }
+        if (world.sphere.pos.y > Main.height && world.sphere.pos.x <= world.sphere.xPos1) {
+            g.drawImage(loseMenu, 0, 0, Main.width, Main.height, null);
+        }
 
-            if (world.sphere.pos.y > Main.height && world.sphere.pos.x >= world.sphere.xPos2) {
-                g.drawImage(winMenu, 0, 0, Main.width, Main.height, null);
-            }
-            System.out.println(mousepressed);
+        if (world.sphere.pos.y > Main.height && world.sphere.pos.x >= world.sphere.xPos2) {
+            g.drawImage(winMenu, 0, 0, Main.width, Main.height, null);
+        }
+        System.out.println(mousepressed);
+        g.setColor(new Color(0));
+        g.drawString("TIME: "+tmp, 50,50);
 
     }
 
